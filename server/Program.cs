f74682660f9e2d6bfc12140b7a676e4eeaa29318
farrahalyssa <idcfarrah@gmail.com>
2025-05-27 10:29:00 -0400

    Added client files

---
+++ server/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DBConnection
+{
+    class Program{
+        static void Main(string[] args){
+            using(var client = new SqlConnection("connectionString")){
+                client.Open();
+                SqlCommand cmd = new SqlCommand("SELECT SOMETHING")
+            }
+        }
+    }
+}
