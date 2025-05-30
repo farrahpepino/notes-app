8c20a86e6334bf932359d59464e496b1361dfa1c
farrahalyssa <idcfarrah@gmail.com>
2025-05-30 18:11:41 -0400

    added sqlconn

--- NotesAppAPI/Program.cs
+++ NotesAppAPI/Program.cs
@@ -1,30 +1,27 @@
-using Microsoft.EntityFrameworkCore.InMemory;
 using NotesAppAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddDbContext<ApiContext>
-    (opt = > opt.UseInMemoryDatabase("NotesDb"));
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
-bbuilder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Add services 
+builder.Services.AddSingleton<DapperContext>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("AllowAll"); // Enable CORS
+app.MapControllers(); // Map controllers to handle requests
+app.Run(); // Start the application
